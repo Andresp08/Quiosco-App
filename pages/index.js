b572892ff92e8120383aa99297c9cc267d804252
Andresp08 <Andresfp29032001@gmail.com>
2022-07-14 14:27:24 -0500

    building database model and testing connection

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
+import { PrismaClient } from '@prisma/client'
 
-export default function Home() {
+export default function Home({ categorias }) {
+  console.log(categorias)
   return (
-    <h1 className="text-3xl font-bold underline text-red-700">
-      Hello world!
-    </h1>
-  )
+    <h1 className='text-3xl font-bold underline text-red-700'>Hello world!</h1>
+  );
 }
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient();
+
+  const categorias = await prisma.categoria.findMany();
+
+  return {
+    props: {
+      categorias,
+    },
+  };
+};
